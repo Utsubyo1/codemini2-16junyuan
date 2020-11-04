b7daba722ed0e322ebffc721d2d6b8486ec1e9ab
ustubyo <72735425+ustubyo@users.noreply.github.com>
2020-11-04 16:16:21 +0800

    Initial commit

---
+++ Assets/script/cubemovecontroller.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubemovecontroller : MonoBehaviour
+{
+    //move speed
+    float forwardSpeed = 10.0f;
+    float zlimit = 27.5f;
+    float zlimit2 = -1f;
+    bool reachz = false;
+    public GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (transform.position.z < zlimit && reachz == false)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * forwardSpeed);
+            
+        }
+       if ( transform.position.z > zlimit)
+        {
+            reachz = true;
+        }
+        if (reachz == true)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * -forwardSpeed);
+        }
+        if (transform.position.z < zlimit2)
+        {
+            reachz = false;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            Player.transform.parent = transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+      {
+          if (other.gameObject == Player)
+          {
+              Player.transform.parent = null;
+          }
+      }
+}
---
+++ Assets/script/playercontroller.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour
+{
+    float speed = 10.0f;
+    float maxlimit = 5.0f;
+    float gravityModifuer = 2.5f;
+    float jumpforce = 13.0f;
+
+    Rigidbody playerRb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifuer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalinput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+
+        //Move Player (Gameobject) according to user interaction
+        transform.Translate(Vector3.forward * Time.deltaTime * verticalinput * speed);
+      //  transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
+        Playerjump();
+        
+    }
+    private void Playerjump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
+
+        }
+    }
+
+   
+
+}
